2f833d018727114282daec26fb80894f9b87d356
LaForest <Scott_LaForest@baylor.edu>
2019-03-21 16:59:44 -0500

    added tests to setup.py

--- nba_tweepy/tests/test_nba.py
+++ nba_tweepy/tests/test_nba.py
@@ -2,12 +2,22 @@ from unittest import TestCase
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+import datetime as dt
+
 import nba_tweepy as nt
 import credentials as creds
-
+nba = nt.NBA(creds.consumer_key, creds.consumer_secret, creds.access_key, creds.access_secret)
 
 
 class TestNba(TestCase):
     def test_is_auth(self):
-        nba = nt.NBA(creds.consumer_key, creds.consumer_secret, creds.access_key, creds.access_secret)
+        
         self.assertEqual(nba.api.me().screen_name, creds.myScreenName)
+
+    def test_update_player_list(self):
+        # player_csv_modified_date = os.path.getmtime(os.path.join(os.path.dirname(os.path.dirname(__file__),'data/player-list.csv')))
+        nt.NBA.update_player_list()
+        player_list_mod_date = dt.date.fromtimestamp(os.path.getmtime('data/player-list.csv'))
+        today = dt.datetime.today()
+        self.assertEqual(player_list_mod_date, today)
+        pass
--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/scottyla19/nba-tweepy",
     packages=setuptools.find_packages(),
+    test_suite='nose.collector',
+    tests_require=['nose'],
     install_requires=[
           'tweepy',
           'pandas'
